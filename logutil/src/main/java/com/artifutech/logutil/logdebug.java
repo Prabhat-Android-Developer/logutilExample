4e54108f828a79336e333d782b22e4e91a330809
Prabhat-Android-Developer <Prabhat@artifutech.com>
2022-08-22 15:12:13 +0530

    Initial commit

---
+++ logutil/src/main/java/com/artifutech/logutil/logdebug.java
@@ -0,0 +1,13 @@
+package com.artifutech.logutil;
+
+import android.util.Log;
+
+public class logdebug {
+
+    private static final String TAG ="logutil";
+    public static void e(String message){
+
+        Log.d(TAG, "is working");
+
+    }
+}
